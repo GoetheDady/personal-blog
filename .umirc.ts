bdc73865204f5a86b2016d15ccfcc9fcb673fac4
gedesiwen <gedesw@chanjet.com>
2021-11-08 13:49:57 +0800

    feat: fix router public path

--- .umirc.ts
+++ .umirc.ts
@@ -3,5 +3,6 @@ import { defineConfig } from 'dumi';
 export default defineConfig({
   title: 'Site Name',
   mode: 'site',
+  publicPath: '/personal-blog/',
   // more config: https://d.umijs.org/config
 });
